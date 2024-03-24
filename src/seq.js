009fa5693af18bc2e3162a1f8743278e30801ab3
wuth <906650474@qq.com>
2024-03-24 09:18:41 +0800

    feat: seq

--- src/seq.js
+++ src/seq.js
@@ -4,11 +4,21 @@ const config = {
   host: 'localhost',
   dialect: 'mysql',
 };
+config.pool = {
+  max: 5, // 连接池中最大的链接数量
+  min: 0, 
+  idle: 10000 // 如果一个连接池 10s 之内没被使用，则释放
+}
 
 const seq = new Sequelize('koa2_weibo_db', 'root', 'root123456', config );
 
 
-
+// 测试链接
+// seq.authenticate().then(() => {
+//   console.log('auth ok')
+// }).catch(() => {
+//   console.log('auth err')
+// })
 
 
 module.exports = seq;
